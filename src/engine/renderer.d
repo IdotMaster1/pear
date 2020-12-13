98ad7bc8adaf69c2679078fbe64d5228ccd552bb
Abb1x <loicgre@gmail.com>
2020-12-12 19:43:05 -0500

    Cleaned code + game loop

---
+++ src/engine/renderer.d
@@ -0,0 +1,3 @@
+src/engine/renderer.o: src/engine/renderer.cpp \
+ src/engine/../inc/renderer.hpp
+src/engine/../inc/renderer.hpp:
---
+++ src/main.d
@@ -0,0 +1,5 @@
+src/main.o: src/main.cpp src/inc/renderer.hpp src/inc/engine.hpp \
+ src/inc/renderer.hpp
+src/inc/renderer.hpp:
+src/inc/engine.hpp:
+src/inc/renderer.hpp:
